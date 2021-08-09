965c8eac6be7546b3740f2f9b233afb003a81254
Takumi Kanada <candy12t@gmail.com>
2021-08-09 13:58:39 +0900

    2.4: Define Parser

---
+++ parser/parser.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"github.com/candy12t/monkey/ast"
+	"github.com/candy12t/monkey/lexer"
+	"github.com/candy12t/monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParserProgram() *ast.Program {
+	return nil
+}
