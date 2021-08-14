f1e4e6568e0eec7a751c598fad0b19c773a818d6
Takumi Kanada <candy12t@gmail.com>
2021-08-14 20:58:45 +0900

    Fix typo…

--- ast/ast.go
+++ ast/ast.go
@@ -26,7 +26,7 @@ type Program struct {
 	Statements []Statement
 }
 
-func (p *Program) TokenLitetal() string {
+func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
 	} else {
